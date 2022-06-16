fe6d4c1cc50775ed118e24d4a47ba41833f39497
Willbeckh <wileyhndirangu04@gmail.com>
2022-06-16 21:37:22 +0300

    ft; create login component -- login template

--- src/app/components/login/login.component.ts
+++ src/app/components/login/login.component.ts
@@ -1,40 +1,39 @@
 import { HttpClient } from '@angular/common/http';
 import { Component, NgZone, OnInit } from '@angular/core';
-import { FormBuilder, FormGroup } from '@angular/forms';
+import { FormArray, FormBuilder, FormGroup } from '@angular/forms';
 import { Router } from '@angular/router';
 import { UserService } from 'src/app/user.service';
-
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.css'],
 })
 export class LoginComponent implements OnInit {
-  // loginForm: FormGroup;
+  loginForm: FormGroup;
   // userArray: any = [];
-  // constructor(
-  //   public fb: FormBuilder,
-  //   private ngZone: NgZone,
-  //   private router: Router,
-  //   public userService: UserService
-  // ) {}
 
+  constructor(
+    public fb: FormBuilder,
+    public userService: UserService,
+    private router: Router,
+    private ngZone: NgZone
+  ) {}
   ngOnInit(): void {
-    // this.loginUser();
+    this.loginUser();
   }
 
   // login user
-  // loginUser() {
-  //   this.loginForm = this.fb.group({
-  //     // form input for username + password
-  //     username: [''],
-  //     password: [''],
-  //   });
-  // }
-  // submitForm() {
-  //   this.userService.loginUser(this.loginForm.value).subscribe((data) => {
-  //     console.log('Logged in');
-  //     this.ngZone.run(() => this.router.navigateByUrl('/'));
-  //   });
-  // }
+  loginUser() {
+    this.loginForm = this.fb.group({
+      // form input for username + password
+      username: [''],
+      password: [''],
+    });
+  }
+  submitForm() {
+    this.userService.loginUser(this.loginForm.value).subscribe((data) => {
+      console.log('Logged in');
+      this.ngZone.run(() => this.router.navigateByUrl('/'));
+    });
+  }
 }
