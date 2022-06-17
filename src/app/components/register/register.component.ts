2ab86c69762f46ea7a6d9630c07154c420fedfff
Willbeckh <wileyhndirangu04@gmail.com>
2022-06-17 14:37:16 +0300

    update register component

--- src/app/components/register/register.component.ts
+++ src/app/components/register/register.component.ts
@@ -1,15 +1,45 @@
-import { Component, OnInit } from '@angular/core';
+import { ThisReceiver } from '@angular/compiler';
+import { Component, NgZone, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
+import { UserService } from 'src/app/user.service';
 
 @Component({
   selector: 'app-register',
   templateUrl: './register.component.html',
-  styleUrls: ['./register.component.css']
+  styleUrls: ['./register.component.css'],
 })
 export class RegisterComponent implements OnInit {
+  registerForm: FormGroup;
 
-  constructor() { }
+  constructor(
+    public fb: FormBuilder,
+    public userService: UserService,
+    private router: Router,
+    private ngZone: NgZone
+  ) {}
 
   ngOnInit(): void {
+    this.registerUser();
   }
+  // CREATE USER
+  registerUser() {
+    this.registerForm = this.fb.group({
+      username: [''],
+      first_name: [''],
+      last_name: [''],
+      email: [''],
+      password: [''],
+      password2: [''],
+      addDynamicElement: this.fb.array([]),
+    });
+  }
+  // SUBMIT FORM
+  submitForm() {
+    this.userService.registerUser(this.registerForm.value).subscribe((data) => {
+      console.log('user registered', data);
 
+      this.ngZone.run(() => this.router.navigateByUrl('/login'));
+    });
+  }
 }
