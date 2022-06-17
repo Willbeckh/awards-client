59535ac7852a87cda006bf118223b3501e2f0026
Willbeckh <wileyhndirangu04@gmail.com>
2022-06-17 18:53:02 +0300

    remove unused code in register component

--- src/app/components/register/register.component.ts
+++ src/app/components/register/register.component.ts
@@ -1,4 +1,3 @@
-import { ThisReceiver } from '@angular/compiler';
 import { Component, NgZone, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { Router } from '@angular/router';
