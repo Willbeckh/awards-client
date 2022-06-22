30d4ccec188c50398aceec0bdbf92c1d09b8a3e6
Willbeckh <wileyhndirangu04@gmail.com>
2022-06-22 15:53:27 +0300

    update new project component

--- src/app/components/new-project/new-project.component.ts
+++ src/app/components/new-project/new-project.component.ts
@@ -1,9 +1,9 @@
-import { Component, OnInit } from '@angular/core';
-import { FormArray, FormBuilder, FormGroup } from '@angular/forms';
-import { UserService } from 'src/app/user.service';
+import { ChangeDetectorRef, Component, OnInit } from '@angular/core';
+import { FormBuilder, FormControl, FormGroup } from '@angular/forms';
 import { Router } from '@angular/router';
 import { NgZone } from '@angular/core';
-import { ChangeDetectorRef, ElementRef, ViewChild } from '@angular/core';
+import { ProjectService } from 'src/app/project.service';
+import { UserService } from 'src/app/user.service';
 
 @Component({
   selector: 'app-new-project',
@@ -11,22 +11,45 @@ import { ChangeDetectorRef, ElementRef, ViewChild } from '@angular/core';
   styleUrls: ['./new-project.component.css'],
 })
 export class NewProjectComponent implements OnInit {
+  selectedFile: File;
   projectForm: FormGroup;
 
   constructor(
     private fb: FormBuilder,
+    public projectService: ProjectService,
     private router: Router,
-    private ngZone: NgZone,
-    private cd: ChangeDetectorRef,
-    public userService: UserService
+    private ngZone: NgZone
   ) {}
 
-  ngOnInit(): void {
-    this.onSubmit();
+  get f() {
+    return this.projectForm.controls;
   }
 
-  // upload image on button click
-  onSubmit() {}
+  ngOnInit() {
+    this.projectForm = this.fb.group({
+      author: 1,
+      name: [''],
+      description: [''],
+      image_file: [''],
+      url: [''],
+    });
+    console.log(this.projectForm.value.image_file);
+  }
 
-  uploadFile(event) {}
+  onUploadFile(event: any) {
+    // const reader = new FileReader/();
+    this.selectedFile = event.target.files[0].name;
+  }
+
+  onSubmit() {
+    console.log(this.projectForm.value);
+    this.projectForm.value.image_file = this.selectedFile;
+    console.log(this.projectForm.value.image_file);
+
+    this.projectService
+      .createProject(this.projectForm.value)
+      .subscribe((data) => {
+        this.ngZone.run(() => this.router.navigateByUrl('/'));
+      });
+  }
 }
